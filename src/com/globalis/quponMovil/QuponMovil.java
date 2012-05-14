459c57b9dd01cf11e23dbb4250680d6c16f3c349
unknown <aeliseche@DEV28.omnitronic.com.ar>
2012-05-14 13:42:06 -0300

    Start-up

---
+++ src/com/globalis/quponMovil/QuponMovil.java
@@ -0,0 +1,13 @@
+package com.globalis.quponMovil;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuponMovil extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
