bde198c4b36df79f078ff032d1157edfef4c739d
Federico Cara <potter_federico@hotmail.com>
2012-07-06 13:16:04 -0300

    Terminar Modificar informacion de usuario

--- src/com/globalis/network/HttpRequest.java
+++ src/com/globalis/network/HttpRequest.java
@@ -13,234 +13,298 @@ import java.util.Hashtable;
 import java.util.Map.Entry;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+
+import com.google.gson.Gson;
 
 public class HttpRequest {
 	private String initialUrl;
 	private Hashtable<String, String> initialParams;
 	private HttpMethod initialHttpMethod;
 	private URL url;
-	private String params;
-	
+	private String params, jsonParams;
+
 	public enum HttpMethod {
-		GET, POST;
+		GET, POST, POSTJSON, PUT;
 	}
-	
+
 	public static class Url {
-		public static String base = "http://50.116.21.186";		
-		//static {base = "http://192.168.0.196:3000";}
-		
+		public static String base = "http://50.116.21.186";
+
+		 static {base = "http://192.168.0.196:3000";}
+
 		public static String getBase() {
 			return base;
 		}
-		
-		public static String getLogin() {			 
-			return base + "/api/v1/tokens.json";	
+
+		public static String getLogin() {
+			return base + "/api/v1/tokens.json";
 		}
-		
+
 		public static String getPromotion() {
 			return base + "/promotions.json";
 		}
-		
-		public static String getPromotion(int promotionID){
-			return base + "/promotions/"+promotionID+".json";
+
+		public static String getPromotion(int promotionID) {
+			return base + "/promotions/" + promotionID + ".json";
 		}
-		
+
 		public static String getSignup() {
 			return base + "/d/users.json";
 		}
-		
-		public static String getUserInfo(){
+
+		public static String getUserInfo() {
 			return base + "/d/users/edit.json";
 		}
-		
+
 		public static String getCoupons(int promotionID) {
 			return base + "/promotions/" + promotionID + "/coupons.json";
 		}
-		
-		public static String getCoupon(int couponID){
-			return base + "coupons/"+ couponID;
+
+		public static String getCoupon(int couponID) {
+			return base + "coupons/" + couponID;
 		}
-		
-		public static String getMyCoupons(){
+
+		public static String getMyCoupons() {
 			return base + "/coupons/my.json";
 		}
-		
+
 	}
-	
-	public void set(String initialUrl, Hashtable<String, String> initialParams, HttpMethod initialHttpMethod) {
+
+	public void set(String initialUrl, Object initialParams,
+			HttpMethod initialHttpMethod) {
 		this.initialUrl = initialUrl;
-		this.initialParams = initialParams;
+		if (initialParams instanceof Hashtable<?, ?>) {
+			this.initialParams = (Hashtable<String, String>) initialParams;
+			this.jsonParams = null;
+		} else if (initialParams instanceof String) {
+			this.jsonParams = (String) initialParams;
+			this.initialParams = null;
+		} else {
+			this.initialParams = null;
+			this.jsonParams = null;
+		}
 		this.initialHttpMethod = initialHttpMethod;
 	}
-	
+
 	public Response execute() throws Exception {
-		if(this.initialHttpMethod == HttpMethod.GET) {
-			return get(this.initialUrl, this.initialParams);			
+		if (this.initialHttpMethod == HttpMethod.GET) {
+			return get(this.initialUrl, this.initialParams);
+		} else if (this.initialHttpMethod == HttpMethod.POST) {
+			return post(this.initialUrl, this.initialParams);
+		} else if(this.initialHttpMethod == HttpMethod.POSTJSON){
+			return post(this.initialUrl, this.jsonParams);
+		}else if (this.initialHttpMethod == HttpMethod.PUT) {
+			return put(this.initialUrl, this.jsonParams);
 		}
-		else if(this.initialHttpMethod == HttpMethod.POST) {
-			return post(this.initialUrl, this.initialParams);			
-		}
-		
+
 		return null;
-	}	
-	
-	public Response get(String url, Hashtable<String, String> params) throws Exception {
+	}
+
+
+	public Response get(String url, Hashtable<String, String> params)
+			throws Exception {
 		setParams(params);
-		if(this.params.length() != 0) {
-			url = url + "?" + this.params;			
+		if (this.params.length() != 0) {
+			url = url + "?" + this.params;
 		}
-		setUrl(url);		
-		
+		setUrl(url);
+
 		HttpURLConnection conn = null;
 		Response response = null;
-		
+
 		try {
-			conn = (HttpURLConnection)this.url.openConnection();
+			conn = (HttpURLConnection) this.url.openConnection();
 			InputStream in = conn.getInputStream();
-			byte[] body = readStream(in);			
+			byte[] body = readStream(in);
 			response = new Response(conn.getResponseCode(), new String(body));
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new ConnectionErrorException();
-		} catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new ConnectionErrorException();
-		}
-		finally {
-			if(conn != null) {
+		} finally {
+			if (conn != null) {
 				conn.disconnect();
 			}
 		}
 		return response;
 	}
-	
-	public Response post(String url, Hashtable<String, String> params) throws Exception {		
+
+	public Response post(String url, Hashtable<String, String> params)
+			throws Exception {
 		ArrayList<NameValuePair> postParams = new ArrayList<NameValuePair>();
-	    for (Entry<String, String> entry : params.entrySet()) {
-	    	postParams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));	        
-	    }
-		
+		for (Entry<String, String> entry : params.entrySet()) {
+			postParams.add(new BasicNameValuePair(entry.getKey(), entry
+					.getValue()));
+		}
+
 		HttpClient conn = null;
 		Response response = null;
-		
+
 		try {
 			conn = new DefaultHttpClient();
-			HttpPost post = new HttpPost(url);			
+			HttpPost post = new HttpPost(url);
 			post.setEntity(new UrlEncodedFormEntity(postParams));
 			HttpResponse res = conn.execute(post);
 			InputStream in = res.getEntity().getContent();
-			byte[] body = readStream(in);			
-			response = new Response(res.getStatusLine().getStatusCode(), new String(body));			
+			byte[] body = readStream(in);
+			response = new Response(res.getStatusLine().getStatusCode(),
+					new String(body));
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new ConnectionErrorException();
 		}
-			
-	    return response;
-	}	
-	
-	/*
-	 *  Better option, but not being used because of a bug when handling error code 401, throws IOException
-	 *  so we decided to use the Apache post.
-	 */
-	/*public Response post(String url, Hashtable<String, String> params) throws Exception {
-		setParams(params);
-		setUrl(url);				
-		
-		HttpURLConnection conn = null;
-		Response response = null;
-		
-		try {			
-			conn = (HttpURLConnection)this.url.openConnection();
-			// Sets the output to true, indicating that it's going to send POST data
-			conn.setDoOutput(true);
-			// Once it sets the output, it's not necessary to do that. Doing anyway
-			conn.setRequestMethod("POST");
-			// Documentation suggest to set the length of the data we are sending to the server
-			conn.setFixedLengthStreamingMode(this.params.getBytes().length);
-			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			// Sends POST out
-			PrintWriter out = new PrintWriter(conn.getOutputStream());
-			out.print(this.params);
-			out.close();
-			InputStream in = conn.getInputStream();
-			byte[] body = readStream(in);			
-			response = new Response(conn.getResponseCode(), new String(body));
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new ConnectionErrorException();
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new ConnectionErrorException();
-		}
-		finally {
-			if(conn != null) {
-				conn.disconnect();
-			}
-		}
-		return response;		
-	}*/
-	
-	private void setProxy() {
-		Authenticator.setDefault(new ProxyAuthenticator(ProxyAuthenticator.USER, ProxyAuthenticator.PASSWORD));		
-		System.setProperty("http.proxyHost", ProxyAuthenticator.HOST);
-		System.setProperty("http.proxyPort", ProxyAuthenticator.PORT);		
+
+		return response;
 	}
 	
-	private void setUrl(String url) {		
+	private Response post(String url, String json) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Response put(String url, Hashtable<String, String> params) {
+		ArrayList<NameValuePair> postParams = new ArrayList<NameValuePair>();
+		for (Entry<String, String> entry : params.entrySet()) {
+			postParams.add(new BasicNameValuePair(entry.getKey(), entry
+					.getValue()));
+		}
+
+		HttpClient conn = null;
+		Response response = null;
+		try {
+			conn = new DefaultHttpClient();
+			HttpPut put = new HttpPut(url);
+			put.setEntity(new UrlEncodedFormEntity(postParams));
+			HttpResponse httpResponse = conn.execute(put);
+			InputStream in = httpResponse.getEntity().getContent();
+			byte[] body = readStream(in);
+			response = new Response(httpResponse.getStatusLine()
+					.getStatusCode(), new String(body));
+			return response;
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
+
+	public Response put(String url, String json) {
+		Response response = null;
+		HttpClient conn = null;
+		try {
+			conn = new DefaultHttpClient();
+			HttpPut put = new HttpPut(url);
+			put.setEntity(new StringEntity(json));
+			put.setHeader("Accept", "application/json");
+			put.setHeader("Content-type", "application/json");
+			HttpResponse httpResponse = conn.execute(put);
+			InputStream in = httpResponse.getEntity().getContent();
+			byte[] body = readStream(in);
+			response = new Response(httpResponse.getStatusLine()
+					.getStatusCode(), new String(body));
+			return response;
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return response;
+	}
+
+	/*
+	 * Better option, but not being used because of a bug when handling error
+	 * code 401, throws IOException so we decided to use the Apache post.
+	 */
+	/*
+	 * public Response post(String url, Hashtable<String, String> params) throws
+	 * Exception { setParams(params); setUrl(url);
+	 * 
+	 * HttpURLConnection conn = null; Response response = null;
+	 * 
+	 * try { conn = (HttpURLConnection)this.url.openConnection(); // Sets the
+	 * output to true, indicating that it's going to send POST data
+	 * conn.setDoOutput(true); // Once it sets the output, it's not necessary to
+	 * do that. Doing anyway conn.setRequestMethod("POST"); // Documentation
+	 * suggest to set the length of the data we are sending to the server
+	 * conn.setFixedLengthStreamingMode(this.params.getBytes().length);
+	 * conn.setRequestProperty("Content-Type",
+	 * "application/x-www-form-urlencoded"); // Sends POST out PrintWriter out =
+	 * new PrintWriter(conn.getOutputStream()); out.print(this.params);
+	 * out.close(); InputStream in = conn.getInputStream(); byte[] body =
+	 * readStream(in); response = new Response(conn.getResponseCode(), new
+	 * String(body)); } catch (IOException e) { e.printStackTrace(); throw new
+	 * ConnectionErrorException(); } catch (Exception e) { e.printStackTrace();
+	 * throw new ConnectionErrorException(); } finally { if(conn != null) {
+	 * conn.disconnect(); } } return response; }
+	 */
+
+	private void setProxy() {
+		Authenticator.setDefault(new ProxyAuthenticator(
+				ProxyAuthenticator.USER, ProxyAuthenticator.PASSWORD));
+		System.setProperty("http.proxyHost", ProxyAuthenticator.HOST);
+		System.setProperty("http.proxyPort", ProxyAuthenticator.PORT);
+	}
+
+	private void setUrl(String url) {
 		try {
 			this.url = new URL(url);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void setParams(Hashtable<String, String> params) {
-		if(params == null) {
+		if (params == null) {
 			this.params = "";
 			return;
-		}
-		else {
-			if(params.size() == 0) {
+		} else {
+			if (params.size() == 0) {
 				this.params = "";
 				return;
 			}
 		}
-		
+
 		StringBuffer buf = new StringBuffer();
 		Enumeration<String> keys = params.keys();
-		while(keys.hasMoreElements()) {
+		while (keys.hasMoreElements()) {
 			buf.append(buf.length() == 0 ? "" : "&");
 			String key = keys.nextElement();
 			buf.append(key).append("=").append(params.get(key));
 		}
 		this.params = buf.toString();
 	}
-	
+
 	private byte[] readStream(InputStream in) throws IOException {
 		byte[] buf = new byte[1024];
 		int count = 0;
 		ByteArrayOutputStream out = new ByteArrayOutputStream(1024);
-		while((count = in.read(buf)) != -1) {
+		while ((count = in.read(buf)) != -1) {
 			out.write(buf, 0, count);
-		}		
+		}
 		return out.toByteArray();
 	}
-	
-	private class ConnectionErrorException extends Exception
-	{
+
+	private class ConnectionErrorException extends Exception {
 		String errorMessage;
-		
+
 		ConnectionErrorException() {
 			super();
-			errorMessage = new String("Error when trying to bring data from network");
+			errorMessage = new String(
+					"Error when trying to bring data from network");
 		}
-		
+
 		@Override
 		public String getMessage() {
 			return errorMessage;
--- src/com/globalis/quponMovil/SignupActivity.java
+++ src/com/globalis/quponMovil/SignupActivity.java
@@ -1,5 +1,6 @@
 package com.globalis.quponMovil;
 
+import java.io.Serializable;
 import java.util.Hashtable;
 
 import com.globalis.entities.JsonResponse;
@@ -10,6 +11,8 @@ import com.globalis.network.Response;
 import com.globalis.quponMovil.CouponDetailActivity.CouponJson;
 import com.globalis.utils.Utils;
 import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -24,62 +27,132 @@ import android.widget.Toast;
 
 public class SignupActivity extends Activity implements OnClickListener {
 	private String email, password, rePassword, firstName, lastName,
-		phoneNumber, zipCode, gender;
-	private EditText txtEmail, txtPassword, txtRePassword, txtFirstName, txtLastName,
-		txtPhoneNumber, txtZipCode;
+			phoneNumber, zipCode, gender;
+	private int userID;
+	private EditText txtEmail, txtPassword, txtRePassword, txtFirstName,
+			txtLastName, txtPhoneNumber, txtZipCode;
 	private Spinner spnGender;
-	private Button btnSignUp;	
-	
+	private Button btnSignUp;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.signup);
-		
-		initViews();		
+
+		initViews();
 	}
-	
+
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.signup_btn_sign_up:
 			collectUserData();
-			if(validateFields()) {
-				User newUser = createUser();
-				saveUser(newUser);
+			if (validateFields()) {
+				if (!LoginActivity.isLogged(this)) {
+					User newUser = createUser();
+					saveUser(newUser);
+				} else {
+					User user = createUser();
+					editUser(user);
+				}
 			}
 			break;
 		default:
 			break;
-		}		
+		}
 	}
-	
+
 	public void saveUser(final User user) {
+		//set the gender in Masculino of Femenino
+		if (user.getGender().equals(getResources().getStringArray(R.array.gender)[1])) {
+			user.setGender("Masculino");
+		} else if (user.getGender().equals(getResources().getStringArray(R.array.gender)[2])) {
+			user.setGender("Femenino");
+		}
+		
+		UserJson userJson = new UserJson();
+		userJson.setUser(user);
+		Gson gson = new Gson();
+		String userString = gson.toJson(userJson).toString();
+		
 		HttpRequest req = new HttpRequest();
-		Hashtable<String, String> params = new Hashtable<String, String>();
-		params.putAll(user.getHashtable());
-		req.set(HttpRequest.Url.getSignup(), params, HttpRequest.HttpMethod.POST);		
-		HttpTask task = new HttpTask() {			
+		req.set(HttpRequest.Url.getSignup(), userString,
+				HttpRequest.HttpMethod.POSTJSON);
+		HttpTask task = new HttpTask() {
 			@Override
 			public void doWork(Response response) {
-				if(response != null) {
+				if (response != null) {
 					Gson gson = new Gson();
-					SignUpResponse signUpResponse = gson.fromJson(response.getBody(), SignUpResponse.class);
-					if(signUpResponse.equals(getResources().getString(R.string.success))) {
-						Toast.makeText(SignupActivity.this, signUpResponse.getMessage(), Toast.LENGTH_LONG).show();						
-						Intent intent = new Intent();                   
-                        intent.putExtra("username", user.getEmail());
-                        intent.putExtra("password", user.getPassword());
-                        setResult(Activity.RESULT_OK, intent);
-                        finish();
+					SignUpResponse signUpResponse = gson.fromJson(
+							response.getBody(), SignUpResponse.class);
+					if (signUpResponse.equals(getResources().getString(
+							R.string.success))) {
+						Toast.makeText(SignupActivity.this,
+								signUpResponse.getMessage(), Toast.LENGTH_LONG)
+								.show();
+						Intent intent = new Intent();
+						intent.putExtra("username", user.getEmail());
+						intent.putExtra("password", user.getPassword());
+						setResult(Activity.RESULT_OK, intent);
+						finish();
+					} else {
+						Toast.makeText(SignupActivity.this,
+								signUpResponse.getMessage(), Toast.LENGTH_LONG)
+								.show();
 					}
-					else{
-						Toast.makeText(SignupActivity.this, signUpResponse.getMessage(), Toast.LENGTH_LONG).show();
-					}
-				}				
+				}
 			}
 		};
 		task.set(SignupActivity.this, req).execute();
 	}
-	
+
+	private void editUser(User user) {
+		//set the gender in Masculino of Femenino
+		if (user.getGender().equals(getResources().getStringArray(R.array.gender)[1])) {
+			user.setGender("Masculino");
+		} else if (user.getGender().equals(getResources().getStringArray(R.array.gender)[2])) {
+			user.setGender("Femenino");
+		}
+		
+		
+		UserEdit userEdit = new UserEdit();
+		userEdit.setAuthToken(GlobalPreference.getToken());
+		userEdit.setUser(user);
+		Gson gson = new Gson();
+		String userJson = gson.toJson(userEdit).toString();
+		HttpRequest req = new HttpRequest();
+		req.set(HttpRequest.Url.getSignup(), userJson,
+				HttpRequest.HttpMethod.PUT);
+		HttpTask task = new HttpTask() {
+			@Override
+			public void doWork(Response response) {
+				if (response != null) {
+					if (response.isValidStatusCode()) {
+						Gson gson = new Gson();
+						UserJson signUpResponse = gson.fromJson(
+								response.getBody(), UserJson.class);
+						if (signUpResponse.getState().equals(
+								getResources().getString(R.string.success))) {
+							Toast.makeText(getApplicationContext(),
+									getApplicationContext().getResources()
+											.getString(R.string.success_edit),
+									Toast.LENGTH_SHORT).show();
+						} else {
+							Toast.makeText(getApplicationContext(),
+									signUpResponse.getMessage(),
+									Toast.LENGTH_SHORT).show();
+						}
+
+					} else {
+						Toast.makeText(getApplicationContext(),
+								response.getBody(), Toast.LENGTH_SHORT).show();
+					}
+
+				}
+			}
+		};
+		task.set(SignupActivity.this, req).execute();
+	}
+
 	private void collectUserData() {
 		email = String.valueOf(txtEmail.getText());
 		password = String.valueOf(txtPassword.getText());
@@ -88,50 +161,69 @@ public class SignupActivity extends Activity implements OnClickListener {
 		lastName = String.valueOf(txtLastName.getText());
 		phoneNumber = String.valueOf(txtPhoneNumber.getText());
 		zipCode = String.valueOf(txtZipCode.getText());
-		gender = String.valueOf(spnGender.getSelectedItem());		
+		gender = String.valueOf(spnGender.getSelectedItem());
 	}
-	
-	private boolean validateFields() {		
-		if(Utils.isNullOrEmpty(email)) {
-			Toast.makeText(this, getResources().getString(R.string.signup_error_msg_complete_user), Toast.LENGTH_LONG).show();
+
+	private boolean validateFields() {
+		if (Utils.isNullOrEmpty(email)) {
+			Toast.makeText(
+					this,
+					getResources().getString(
+							R.string.signup_error_msg_complete_user),
+					Toast.LENGTH_LONG).show();
 			return false;
 		}
-		if(Utils.isNullOrEmpty(password) || Utils.isNullOrEmpty(rePassword) || !password.equals(rePassword)) {
-			Toast.makeText(this, getResources().getString(R.string.signup_error_msg_unmatching_pass), Toast.LENGTH_LONG).show();
+		if (Utils.isNullOrEmpty(password) || Utils.isNullOrEmpty(rePassword)
+				|| !password.equals(rePassword)) {
+			Toast.makeText(
+					this,
+					getResources().getString(
+							R.string.signup_error_msg_unmatching_pass),
+					Toast.LENGTH_LONG).show();
 			return false;
 		}
-		if(Utils.isNullOrEmpty(firstName)) {
-			Toast.makeText(this,getResources().getString(R.string.signup_error_msg_complete_first_name), Toast.LENGTH_LONG).show();
+		if (Utils.isNullOrEmpty(firstName)) {
+			Toast.makeText(
+					this,
+					getResources().getString(
+							R.string.signup_error_msg_complete_first_name),
+					Toast.LENGTH_LONG).show();
 			return false;
 		}
-		if(Utils.isNullOrEmpty(lastName)) {
-			Toast.makeText(this, getResources().getString(R.string.signup_error_msg_complete_last_name), Toast.LENGTH_LONG).show();
+		if (Utils.isNullOrEmpty(lastName)) {
+			Toast.makeText(
+					this,
+					getResources().getString(
+							R.string.signup_error_msg_complete_last_name),
+					Toast.LENGTH_LONG).show();
 			return false;
 		}
 		return true;
 	}
-	
+
 	private User createUser() {
-		User user = new User(email, firstName, lastName, zipCode, phoneNumber, gender, password, rePassword);		 
+		User user = new User(email, firstName, lastName, zipCode, phoneNumber,
+				gender, password, rePassword);
 		return user;
-	}	
+	}
 
 	private void initViews() {
-		txtEmail = (EditText)findViewById(R.id.signup_txt_username);
-		txtPassword = (EditText)findViewById(R.id.signup_txt_password);
-		txtRePassword = (EditText)findViewById(R.id.signup_txt_repassword);
-		txtFirstName = (EditText)findViewById(R.id.signup_txt_first_name);
-		txtLastName = (EditText)findViewById(R.id.signup_txt_last_name);
-		txtPhoneNumber = (EditText)findViewById(R.id.signup_txt_phone_number);
-		txtZipCode = (EditText)findViewById(R.id.signup_txt_zip_code);
-		btnSignUp = (Button)findViewById(R.id.signup_btn_sign_up);		
-		btnSignUp.setOnClickListener(this);		
-		spnGender = (Spinner)findViewById(R.id.signup_spn_gender);
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_item);
+		txtEmail = (EditText) findViewById(R.id.signup_txt_username);
+		txtPassword = (EditText) findViewById(R.id.signup_txt_password);
+		txtRePassword = (EditText) findViewById(R.id.signup_txt_repassword);
+		txtFirstName = (EditText) findViewById(R.id.signup_txt_first_name);
+		txtLastName = (EditText) findViewById(R.id.signup_txt_last_name);
+		txtPhoneNumber = (EditText) findViewById(R.id.signup_txt_phone_number);
+		txtZipCode = (EditText) findViewById(R.id.signup_txt_zip_code);
+		btnSignUp = (Button) findViewById(R.id.signup_btn_sign_up);
+		btnSignUp.setOnClickListener(this);
+		spnGender = (Spinner) findViewById(R.id.signup_spn_gender);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.gender, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spnGender.setAdapter(adapter);
-		
-		if(LoginActivity.isLogged(this)){
+
+		if (LoginActivity.isLogged(this)) {
 			HttpRequest req = new HttpRequest();
 			Hashtable<String, String> params = new Hashtable<String, String>();
 			params.put("auth_token", GlobalPreference.getToken());
@@ -142,27 +234,30 @@ public class SignupActivity extends Activity implements OnClickListener {
 				public void doWork(Response response) {
 					if (response != null) {
 						Gson gson = new Gson();
-						UserJson user = gson.fromJson(
-								response.getBody(), UserJson.class);
-						if (user.getState().equals(getResources().getString(R.string.success))) {
+						UserJson user = gson.fromJson(response.getBody(),
+								UserJson.class);
+						if (user.getState().equals(
+								getResources().getString(R.string.success))) {
 							completeWithUserData(user.getUser());
 						} else {
-							 Toast.makeText(SignupActivity.this, user.getMessage(),
-							 Toast.LENGTH_LONG).show();
+							Toast.makeText(SignupActivity.this,
+									user.getMessage(), Toast.LENGTH_LONG)
+									.show();
 						}
 					}
 				}
 
-				
 			};
 			task.set(SignupActivity.this, req).execute();
 		}
 	}
-	
+
 	private void completeWithUserData(User user) {
-		SharedPreferences pref = getApplicationContext().getSharedPreferences(GlobalPreference.getLogin(), MODE_PRIVATE);
-		String password = pref.getString(GlobalPreference.getLoginPassword(), null);
-		
+		SharedPreferences pref = getApplicationContext().getSharedPreferences(
+				GlobalPreference.getLogin(), MODE_PRIVATE);
+		String password = pref.getString(GlobalPreference.getLoginPassword(),
+				null);
+
 		txtEmail.setText(user.getEmail());
 		txtPassword.setText(password);
 		txtRePassword.setText(password);
@@ -171,16 +266,16 @@ public class SignupActivity extends Activity implements OnClickListener {
 		txtPhoneNumber.setText(user.getPhoneNumber());
 		txtZipCode.setText(user.getZipCode());
 		String gender = user.getGender();
-		if(gender.equals("Masculino") || gender.equals("Male")){
+		if (gender.equals("Masculino") || gender.equals("Male")) {
 			spnGender.setSelection(1);
-		} else if(gender.equals("Femenino") || gender.equals("Female")){
+		} else if (gender.equals("Femenino") || gender.equals("Female")) {
 			spnGender.setSelection(2);
-		}else{
+		} else {
 			spnGender.setSelection(0);
 		}
 	}
-	
-	private class UserJson extends JsonResponse{
+
+	private class UserJson extends JsonResponse {
 		private User user;
 
 		public User getUser() {
@@ -190,8 +285,33 @@ public class SignupActivity extends Activity implements OnClickListener {
 		public void setUser(User user) {
 			this.user = user;
 		}
-		
+
 	}
-	
-	private class SignUpResponse extends JsonResponse{}
+
+	private class UserEdit implements Serializable {
+		@SerializedName("auth_token")
+		private String authToken;
+		private User user;
+
+		public String getAuthToken() {
+			return authToken;
+		}
+
+		public void setAuthToken(String authToken) {
+			this.authToken = authToken;
+		}
+
+		public User getUser() {
+			return user;
+		}
+
+		public void setUser(User user) {
+			this.user = user;
+		}
+
+	}
+
+	private class SignUpResponse extends JsonResponse {
+
+	}
 }
